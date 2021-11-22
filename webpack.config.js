2035456a99c962f41b036927a28ed627a078e122
Dave Murphy <dave.murphy@plusonerobotics.com>
2021-11-21 19:56:03 -0700

    added webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+    entry: './public/index.js',
+    output: {
+      path: __dirname + '/public/dist',
+      filename: 'bundle.js',
+    },
+    mode: 'production',
+      plugins: [
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        inject: false,
+        fingerprints: false,
+  
+        name: 'Budget-Tracker',
+        short_name: 'BT',
+        description: 'An app that keeps track of your budget online, as well as offline',
+        background_color: '#113e52',
+        theme_color: '#c3e8f7',
+        start_url: '/',
+        display: 'standalone',
+  
+        icons: [
+          {
+            src: path.resolve(__dirname, 'public/icons/icon-512x512.png'),
+            sizes: [192, 512],
+          },
+        ],
+      }),
+    ],
+  };
+  
+  module.exports = config;
