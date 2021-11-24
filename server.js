76475f8a18778d8935475d61e37bcffa98e9b975
Dave Murphy <dave.murphy@plusonerobotics.com>
2021-11-24 11:26:52 -0700

    deploying

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
